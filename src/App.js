ce88fa46c14d3d09a893eadd9e2ec6e70628f113
lnardon <lucaslnardon@gmail.com>
2021-02-17 00:12:52 -0300

    🚧 Start main app structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import useData from "./hooks/useData";
+import styles from "./App.module.scss";
+import MatchCard from "./components/MatchCard";
+
 function App() {
-  return <div className="App"></div>;
+  const data = useData();
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(15);
+  const [games, setGames] = useState(data);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentContainer}>
+        <h1>App</h1>
+        <div className={styles.matchesList}>
+          {games.map((game) => (
+            <MatchCard key={game.id} info={game} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MatchCard/index.js
@@ -0,0 +1,5 @@
+function MatchCard({ info }) {
+  return <div>{info.id}</div>;
+}
+
+export default MatchCard;
